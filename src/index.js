231990d728850ed2f4ab72ab7883ad71a94526ef
Noah Wingate-Dolph <phys1ks@noahs-imac.local>
2020-05-19 23:57:19 -0500

    Why not right?

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
   audience: `/v3/admin/`
 });
 
-const instanceConfig = {
-  baseURL: core.getInput('url', {required: true}),
+/* const instanceConfig = {
+  baseURL: core.getInput('url', { required: true }),
   timeout: parseInt(core.getInput('timeout') || 5000, 10),
   headers: { Authorization: `Ghost ${token}` }
 }
@@ -27,7 +27,7 @@ core.debug('Instance Configuration: ' + JSON.stringify(instanceConfig))
 
 const instance = axios.create(instanceConfig);
 
-(async() => {
+(async () => {
   try {
     const method = core.getInput('method') || METHOD_POST;
     const data = method === METHOD_GET ? undefined : JSON.parse(core.getInput('data') || '{}')
@@ -46,12 +46,10 @@ const instance = axios.create(instanceConfig);
   } catch (error) {
     core.setFailed(JSON.stringify({ code: error.response.code, message: error.response.data }))
   }
-})()
+})() */
 
-
-/* 
 // Make an authenticated request to create a post
-const url = core.getInput('ghost-url', { required: true });
+const url = core.getInput('url', { required: true });
 const headers = { Authorization: `Ghost ${token}` };
 const payload = core.getInput('data', { required: true });
 
@@ -59,4 +57,3 @@ const payload = core.getInput('data', { required: true });
 axios.post(url, payload, { headers })
     .then(response => console.log(response))
     .catch(error => console.error(error));
-*/
