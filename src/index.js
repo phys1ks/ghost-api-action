5f8d074bfb2185989f2a9a1ddedc9fb969815396
Noah Wingate-Dolph <phys1ks@noahs-imac.local>
2020-05-20 00:37:31 -0500

    Change headers

--- src/index.js
+++ src/index.js
@@ -20,10 +20,12 @@ const token = jwt.sign({}, Buffer.from(secret, 'hex'), {
   audience: `/v3/admin/`
 });
 
+const headers = { 'Authorization': `Ghost ${token}` }
+
 const instanceConfig = {
   baseURL: core.getInput('url', { required: true }),
   timeout: parseInt(core.getInput('timeout') || 5000, 10),
-  headers: { 'Authorization': `Ghost ${token}` }
+  headers: { ...headers }
 }
 
 core.debug('Instance Configuration: ' + JSON.stringify(instanceConfig))
